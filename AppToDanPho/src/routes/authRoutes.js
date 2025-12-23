4131ae5a7363fae0258195f1627a77a9f87cab18
TranAnhMinhwibu <zanhminhmc@gmail.com>
2025-12-23 14:11:53 +0700

    delete unnec part

--- AppToDanPho/src/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-// src/routes/authRoutes.js
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/authController');
-
-// Định nghĩa đường dẫn: POST /api/auth/login
-router.post('/login', authController.login);
-
-module.exports = router;
